2a5121e2eef319454949f82890fcdd97b5919a73
KerryAM-R <83262343+KerryAM-R@users.noreply.github.com>
2023-09-15 07:49:58 +0200

    updated netplot colours

--- R/netplot.R
+++ R/netplot.R
@@ -12,6 +12,7 @@
 #' @param alpha_selected Transparency of selected cluster (default = 1)
 #' @param alpha_non_selected Transparency of non-selected clusters (default = 0.5)
 #' @param colour Colour selected = "color_test" or all = "color_all"
+#' @param all.colour Colours all points by: rainbow, random, heat.colors, terrain.colors, topo.colors, hcl.colors and default
 #' @param filter_plot Filter's plot to remove connects grater than # e.g. 2 = 3 or more connections.
 #' @importFrom ggnet ggnet2
 #' @import ggplot2
@@ -19,7 +20,13 @@
 #' @importFrom network as.network
 #' @export
 
-netplot_ClusTCR2 <- function(ClusTCR, filter_plot = 0, Clust_selected=1,selected_col="purple",selected_text_col="black",selected_text_size=3,non_selected_text_size=2, Clust_column_name="cluster", label = c("Name","cluster","CDR3","V_gene","Len"), non_selected_col="grey80",non_selected_text_col="grey40",alpha_selected=1,alpha_non_selected=0.5, colour = "color_test") {
+netplot_ClusTCR2 <- function(ClusTCR, filter_plot = 0, Clust_selected=1,selected_col="purple",selected_text_col="black",selected_text_size=3,non_selected_text_size=2, Clust_column_name="cluster", label = c("Name","cluster","CDR3","V_gene","Len"), non_selected_col="grey80",non_selected_text_col="grey40",alpha_selected=1,alpha_non_selected=0.5, colour = "color_test",all.colour = "default") {
+
+  gg_fill_hue <- function(n) {
+    hues = seq(15, 375, length = n + 1)
+    hcl(h = hues, l = 65, c = 100)[1:n]
+  } # colouring function
+
   net.initial <- ClusTCR[[2]]
   df_clust.initial <- ClusTCR[[1]]
   names(ClusTCR)
@@ -32,7 +39,30 @@ netplot_ClusTCR2 <- function(ClusTCR, filter_plot = 0, Clust_selected=1,selected
   col_unique <- as.data.frame(unique(df_clust[,names(df_clust) %in% Clust_column_name]))
   col_unique <- as.data.frame(col_unique[order(col_unique[,1]),])
   names(col_unique) <- Clust_column_name
-  col_unique$col <- rev(rainbow(dim(col_unique)[1]))
+
+  if (all.colour = "rainbow") {
+    col_unique$col <- rev(rainbow(dim(col_unique)[1]))
+  }
+
+ else if (all.colour = "heat.colors") { #(c("rainbow","random","heat.colors","terrain.colors","topo.colors","hcl.colors"))
+   col_unique$col <- heat.colors(dim(col_unique)[1])
+ }
+
+  else if (all.colour = "terrain.colors") { #(c("rainbow","random","heat.colors","terrain.colors","topo.colors","hcl.colors"))
+    col_unique$col <- col.terrain(dim(col_unique)[1])
+  }
+
+  else if (all.colour = "topo.colors") { #(c("rainbow","random","heat.colors","terrain.colors","topo.colors","hcl.colors"))
+    col_unique$col <- topo.colors(dim(col_unique)[1])
+  }
+
+  else if (all.colour = "hcl.colors") { #(c("rainbow","random","heat.colors","terrain.colors","topo.colors","hcl.colors"))
+    col_unique$col <- hcl.colors(dim(col_unique)[1], palette = "viridis")
+  }
+
+  else {
+    col_unique$col <- gg_fill_hue(dim(col_unique)[1])
+  }
 
   df_clust <- merge(df_clust,col_unique,by = Clust_column_name)
   df_clust <- df_clust[order(df_clust$order),]
