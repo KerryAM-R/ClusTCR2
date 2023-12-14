7c8664d6a5b5c55740d215d8c9df797922407ed5
KerryAM-R <83262343+KerryAM-R@users.noreply.github.com>
2023-12-14 15:59:39 +0100

    Update motif_from_clusterfile.R

--- R/motif_from_clusterfile.R
+++ R/motif_from_clusterfile.R
@@ -1,23 +1,22 @@
 #' Code for plotting the Motif based on a specific CDR3 length and V gene (see [netplot] for ).
 #' @param ClusTCR Cluster file produce from [ClusTCR]
-#' @param Clust_selected Select which cluster to reivew
+#' @param Clust_selected Select which cluster to review
 #' @importFrom  VLF aa.count.function
 #' @importFrom ggseqlogo ggseqlogo
 #' @import ggplot2
 #' @export
 
-Motif_from_cluster_file <- function(ClusTCR, Clust_selected=NULL) {
+Motif_from_cluster_file <- function(ClusTCR, Clust_selected=NULL,selected_cluster_column = "Clust_size_order") {
   source(system.file("Functions","motifStack.functions.R",package = "ClusTCR2"))
   motif_DF <- ClusTCR
   z1 <- as.data.frame(t(as.data.frame(strsplit(motif_DF$CDR3_Vgene,"_"))))
   motif_DF$motif <- z1$V1
 
   motif_DF$len1 <- nchar(motif_DF[,names(motif_DF) %in% "motif"])
+  motif_DF$Clust_size_order2 <- motif_DF[,names(motif_DF) %in% selected_cluster_column]
 
-  if (length(which(motif_DF$Clust_size_order==c(Clust_selected)))>0) {
-
-
-    motif_DF_interest <- motif_DF[motif_DF$Clust_size_order %in% c(Clust_selected),]
+  if (length(which(motif_DF$Clust_size_order2==c(Clust_selected)))>0) {
+    motif_DF_interest <- motif_DF[motif_DF$Clust_size_order2 %in% c(Clust_selected),]
     # subset(motif_DF,motif_DF$cluster==Clust_selected)
     motif <- as.data.frame(t(as.data.frame(strsplit(motif_DF_interest$motif, ""))))
     len <- nchar(unique(motif_DF_interest$motif))[1]
